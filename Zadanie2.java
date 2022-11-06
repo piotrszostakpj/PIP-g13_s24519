c5e285ae4cdff6502ab9084b4145b505ef98d8f0
Piotr Szostak <s24519@pjwstk.edu.pl>
2022-11-06 10:24:16 +0100

    Zadanie2.java

---
+++ Zadanie2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+
+        Scanner L1 = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe (przedzial 1-6");
+        String userName = L1.nextInt();
+        Scanner L2 = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe (przedzial 1-6");
+        String userName = L1.nextInt();
+        System.out.println("Podaj pierwsza liczbe (przedzial 1-6");
+
+
+    }
+}
